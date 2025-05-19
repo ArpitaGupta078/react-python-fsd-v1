c5c5232a8efdc92f713340d1965fb4eb42b73d8d
Ishika <ishiika.ydv@gmail.com>
2025-05-19 11:02:27 +0530

    logout controller

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,7 +1,8 @@
-# routes/auth.py
+
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from ..controllers.login_controller import LoginController
+from ..controllers.logout_controller import LogoutController
 from ..models.user import User
 from ..models.api_response import ApiResponse
 
@@ -15,3 +16,8 @@ class LoginRequest(BaseModel):
 @router.post("/login", response_model=ApiResponse)
 def login(login_request: LoginRequest):
     return LoginController.login(login_request.email, login_request.password)
+
+
+@router.post("/logout", response_model=ApiResponse)
+def logout():
+    return LogoutController.logout()
